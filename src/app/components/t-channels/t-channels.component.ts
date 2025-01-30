6bf9d8a3f583bed3656bb6eb98821699ae90851e
Prohorov Sergey <prohorovsa@intech.rshb.ru>
2025-01-30 18:26:34 +0300

    1st commit

---
+++ src/app/components/t-channels/t-channels.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-t-channels',
+  standalone: true,
+  imports: [],
+  templateUrl: './t-channels.component.html',
+  styleUrl: './t-channels.component.css'
+})
+export class TChannelsComponent {
+
+}
