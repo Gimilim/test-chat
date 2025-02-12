c14726b9001922076a13f9b674a55d6c580966fc
Prohorov Sergey <prohorovsa@intech.rshb.ru>
2025-02-12 16:47:01 +0300

    почистил

--- src/app/guards/permission.guard.ts
+++ src/app/guards/permission.guard.ts
@@ -1,4 +1,4 @@
-import { ActivatedRouteSnapshot, CanActivateFn } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { NavigatorService } from '../services/navigator.service';
 import { UserRepository } from '../components/mainform/t-users/t-users.repository';
